939203c5e7d74cfcc75106628085eb4aa7bef526
Iain MacDonald <IJMacD@gmail.com>
2018-03-12 16:33:29 +0800

    Easier configuration of compiler in build script

--- build.sh
+++ build.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 mkdir -p build && cd build || (echo "Error creating directory" >&2 && exit -1)
 
-rm *.o
-clang -c ../src/*.c ../src/c/*.c
-clang -lm *.o -o calc
+CC=clang
 
-rm *.o
-clang -c ../src/*.c ../src/java/*.c
-clang -lm *.o -o calc-java
+rm -f *.o
+$CC -c ../src/*.c ../src/c/*.c
+$CC -lm *.o -o calc
 
-rm *.o
-clang -c ../src/*.c ../src/python/*.c
-clang -lm *.o -o calc-python
+rm -f *.o
+$CC -c ../src/*.c ../src/java/*.c
+$CC -lm *.o -o calc-java
 
-rm *.o
-clang -c ../src/*.c ../src/llvm/*.c
-clang -lm *.o -o calc-llvm
+rm -f *.o
+$CC -c ../src/*.c ../src/python/*.c
+$CC -lm *.o -o calc-python
 
-rm *.o
-clang -c ../src/*.c ../src/wasm/*.c
-clang -lm *.o -o calc-wasm
+rm -f *.o
+$CC -c ../src/*.c ../src/llvm/*.c
+$CC -lm *.o -o calc-llvm
+
+rm -f *.o
+$CC -c ../src/*.c ../src/wasm/*.c
+$CC -lm *.o -o calc-wasm
